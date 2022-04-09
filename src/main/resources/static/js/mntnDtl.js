738a2334b0e4a23364912896ff10a035222f0204
sushinIm <iss0416@naver.com>
2022-04-09 16:03:24 +0900

    final_update2

--- src/main/resources/static/js/mntnDtl.js
+++ src/main/resources/static/js/mntnDtl.js
@@ -16,16 +16,27 @@ function loadMap(route){
 	getAjax(url, function(){
 		if(this.status == 200){
 			let data = JSON.parse(this.response);
+			let from = new kakao.maps.LatLng(data[0].lat, data[0].lon)
 			let linePath = [];
-			let from = new kakao.maps.LatLng(data[data.length-1].lat, data[data.length-1].lon)
 			
+			mapContainer.innerHTML = "";
+			mapOption = {
+				center: from, //지도의 중심좌표.
+				level: 7 //지도의 레벨(확대, 축소 정도)
+			}
 			
-			map.panTo(from);
+			map = new kakao.maps.Map(mapContainer, mapOption); //지도 생성 및 객체 리턴
+			
+			//map.panTo(from);
 			
 			let marker = new kakao.maps.Marker({
 		    	position: from
 			});
 			
+			let test = new kakao.maps.Marker({
+				position : new kakao.maps.LatLng(37.864410, 127.984306)
+			});
+			test.setMap(map);
 			marker.setMap(map);
 			
 			data.forEach(el => {
@@ -41,36 +52,8 @@ function loadMap(route){
 			})
 			
 			polyline.setMap(map);
-			/* 선을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 선을 표시합니다
-			var linePath = [
-			    new kakao.maps.LatLng(33.452344169439975, 126.56878163224233),
-			    new kakao.maps.LatLng(33.452739313807456, 126.5709308145358),
-			    new kakao.maps.LatLng(33.45178067090639, 126.5726886938753) 
-			];
-			
-			// 지도에 표시할 선을 생성합니다
-			var polyline = new kakao.maps.Polyline({
-			    path: linePath, // 선을 구성하는 좌표배열 입니다
-			    strokeWeight: 5, // 선의 두께 입니다
-			    strokeColor: '#FFAE00', // 선의 색깔입니다
-			    strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-			    strokeStyle: 'solid' // 선의 스타일입니다
-			});
-			
-			// 지도에 선을 표시합니다 
-			polyline.setMap(map);  
-			*/
 		}else{
 			alert(this.status + "에러가 발생했습니다");
 		}
 	});
 }
-
-function panTo() {
-    // 이동할 위도 경도 위치를 생성합니다 
-    var moveLatLon = new kakao.maps.LatLng(33.450580, 126.574942);
-    
-    // 지도 중심을 부드럽게 이동시킵니다
-    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
-    map.panTo(moveLatLon);            
-}   
