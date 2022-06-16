1a6733c0c450840865bf95776be3d7c97648cc35
saeha <saeha@antigravity.kr>
2022-06-16 17:19:58 +0900

    MainController 롤백

--- src/main/java/com/sosam/hiking/controller/MainController.java
+++ src/main/java/com/sosam/hiking/controller/MainController.java
@@ -2,31 +2,32 @@ package com.sosam.hiking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@RestController
+@Controller
 public class MainController {
-	
-	@GetMapping(value = {"/", "/ch"})
-	public void index(HttpServletResponse response) throws IOException {
-		response.sendRedirect("content/main.html");
-	}
-	
-	@GetMapping("/ch/info/mountains")
-	public void infos(HttpServletResponse response) throws IOException {
-		response.sendRedirect("content/hundred.html");
-	}
-	
-	@GetMapping("/ch/statistics/rain")
-	public void rain(HttpServletResponse response) throws IOException {
-		response.sendRedirect("content/rain.html");
+
+	@GetMapping("/")
+	public String index() {
+		return "redirect:/ch";
 	}
 
-	@GetMapping("/ch/statistics/fire")
-	public void fire(HttpServletResponse response)throws IOException {
-		response.sendRedirect("content/fire.html");
+	@GetMapping("/ch")
+	public String home() {
+		return "content/main";
+	}
+
+	@GetMapping("/ch/infos")
+	public String infos() {
+		return "content/hundred";
+	}
+
+	@GetMapping("/ch/rain")
+	public String rain() {
+		return "content/rain";
+	}
+
+	@GetMapping("/ch/fire")
+	public String fire() {
+		return "content/fire";
 	}
 }
